f83dc61f19f31d26549b00b1dfb9d2a459522b59
Tfu1234 <ty.fu@outlook.com>
2025-06-25 23:33:41 -0400

    Add files via upload

---
+++ Currency converter.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+df = pd.read_csv(r"C:\Users\tyfu\Downloads\currency-rates.csv")
+
+while True:
+    
+    while True:
+        currency_start = input("What would you like to convert").strip()
+        result_for_start = df[df['Currency'] == currency_start]
+        
+        if not result_for_start.empty:
+            print(result_for_start)
+            break
+        else:
+            print("No Currency Found")
+
+    section_result_for_start = result_for_start[["Value"]].values[0]
+
+    print(section_result_for_start)
+
+    while True:
+        currency_end = input("Currency You Would like to Convert Too").strip()
+        result_for_end = df[df['Currency'] == currency_end]
+
+        if not result_for_end.empty:
+            print(result_for_end)
+            break
+        else:
+            print("No Currency Found")
+
+    section_result_for_end = result_for_end[["Value"]].values[0]
+
+    print(section_result_for_end)
+
+    product = section_result_for_start * section_result_for_end
+    print("The value is $", product)
